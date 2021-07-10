5695f32f74c2a29f21dbc12166aa3b085b9e836b
nicolaserny <nicolas.erny@xcomponent.com>
2021-07-10 17:04:05 +0200

    Exercice 3

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -1,40 +1,49 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { WEIGHTS } from '../../constants';
+import { QUERIES, WEIGHTS } from "../../constants";
 
-import Breadcrumbs from '../Breadcrumbs';
-import Select from '../Select';
-import Spacer from '../Spacer';
-import ShoeSidebar from '../ShoeSidebar';
-import ShoeGrid from '../ShoeGrid';
+import Breadcrumbs from "../Breadcrumbs";
+import Select from "../Select";
+import Spacer from "../Spacer";
+import ShoeSidebar from "../ShoeSidebar";
+import ShoeGrid from "../ShoeGrid";
+
+const PageBreadCrumbs = (props) => {
+  return (
+    <Breadcrumbs {...props}>
+      <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href="/sale/shoes">Shoes</Breadcrumbs.Crumb>
+    </Breadcrumbs>
+  );
+};
 
 const ShoeIndex = ({ sortId, setSortId }) => {
   return (
     <Wrapper>
       <MainColumn>
         <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
-          </Select>
+          <TitleWrapper>
+            <TabletAndSmallerBreadcrumbs />
+            <Title>Running</Title>
+          </TitleWrapper>
+          <SelectFilterWrapper>
+            <Select
+              label="Sort"
+              value={sortId}
+              onChange={(ev) => setSortId(ev.target.value)}
+            >
+              <option value="newest">Newest Releases</option>
+              <option value="price">Price</option>
+            </Select>
+          </SelectFilterWrapper>
         </Header>
         <Spacer size={32} />
         <ShoeGrid />
       </MainColumn>
       <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
+        <PageBreadCrumbs />
         <Spacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
@@ -51,7 +60,25 @@ const Wrapper = styled.div`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
 `;
+
+const TabletAndSmallerBreadcrumbs = styled(PageBreadCrumbs)`
+  display: none;
+  @media ${QUERIES.tabletAndSmaller} {
+    display: block;
+  }
+`;
+
+const SelectFilterWrapper = styled.div`
+  @media ${QUERIES.phoneAndSmaller} {
+    display: none;
+  }
+`;
+
+const TitleWrapper = styled.div``;
 
 const MainColumn = styled.div`
   flex: 1;
@@ -61,6 +88,9 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+  @media ${QUERIES.tabletAndSmaller} {
+    align-items: flex-end;
+  }
 `;
 
 const Title = styled.h2`
